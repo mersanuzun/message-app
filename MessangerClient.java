58e864bdad85b0d830514e0efc2945c1fb83089b
mersanuzun <mersanuzun@gmail.com>
2016-02-22 13:29:24 +0200

    Gui is designed

---
+++ MessangerClient.java
@@ -0,0 +1,13 @@
+package com.mersanuzun.examples.messanger;
+
+/**
+ * Created by mersanuzun on 2/22/16.
+ */
+public class MessangerClient {
+
+    public static void main(String args[]){
+        MessangerFrame frame = new MessangerFrame();
+        frame.setVisible(true);
+
+    }
+}
---
+++ Messenger.java
@@ -0,0 +1,7 @@
+package com.mersanuzun.examples.messanger;
+
+/**
+ * Created by mersanuzun on 2/22/16.
+ */
+public class Messenger {
+}
